fc64ec69ae021c8b09ede28d2aa70d0b60cf3701
askador <fedosov0912@gmail.com>
2020-08-17 17:34:55 +0300

    stats changes

--- misc.py
+++ misc.py
@@ -4,9 +4,10 @@ from aiogram import Bot, Dispatcher
 import psycopg2
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN = '11111111:AAAAAAAAAAAAA'
+TOKEN = '996503468:AAE8aR09qP8uPdF-322GSr1DTtJUmUBAhmo'
 DB = (
-      "postgres://"
+      "postgres://ldecbdhgnzovuk:223d4e6aeda20ddca3d72f25d4557040ef6b05616a959788096c193d5f70e61b"
+      "@ec2-34-197-188-147.compute-1.amazonaws.com:5432/db5fuj6d41dpo6"
       )
 
 bot = Bot(TOKEN, parse_mode='html')
